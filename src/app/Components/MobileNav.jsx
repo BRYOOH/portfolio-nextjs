58c8e77e41e7722a216c32f9e29f19c7159801e8
BRYOOH <bryoohmuchira@gmail.com>
2024-09-23 23:21:45 +0300

    menu

--- src/app/Components/MobileNav.jsx
+++ src/app/Components/MobileNav.jsx
@@ -1,6 +1,36 @@
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+
+
+const links = [
+  {
+      name:"home",
+      path:"/home",
+  },
+  {
+      name:"experience",
+      path:"/experience",
+  },
+  {
+      name:"skills",
+      path:"/skills",
+  },
+  {
+      name:"projects",
+ 
+      path:"/contact",
+  },];
+
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
+    <Sheet>
+      <SheetTrigger>
+       <Menu className="size-[30px] text-accent " />
+      </SheetTrigger>
+      <SheetContent>
+        <div className="">logo</div>
+      </SheetContent>
+    </Sheet>
   )
 }
 
