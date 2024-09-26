2bdf0438503efa4f139f330d036cbbb85f8010df
BRYOOH <bryoohmuchira@gmail.com>
2024-09-26 07:18:01 +0300

    ProfileFinal

--- src/app/Components/Stats.tsx
+++ src/app/Components/Stats.tsx
@@ -1,7 +1,45 @@
+"use client";
+import CountUp from "react-countup"
+
+const info = [
+  {
+    num:2,
+    name: "Years of experience"
+  }, 
+  {
+    num:8,
+    name: "Technologies learnt"
+  },
+  {
+    num:27,
+    name: "Project completed"
+  },
+  {
+    num:182,
+    name: "Number of Total commits"
+  },
+]; 
 
 const Stats = () => {
   return (
-    <div>Stats</div>
+    <section className="pt-4 pb-12 xl:pb-0 xl:pt-0">
+      <div className="container mx-auto">
+        <div className="flex flex-wrap gap-6 max-w-[80vm] mx-auto xl:max-w-none pb-14 ">
+          {info.map((stat,index)=>{
+            return (
+            <div key={index} className="flex-1 flex gap-4 items-center justify-center xl:justify-start ">
+            <CountUp
+            end={stat.num}
+            duration={6}
+            delay={2}
+            className="text-4xl xl:text-6xl font-extrabold"
+            />
+            <p className={`${stat.name.length<15 ? "max-w-[100px]":"max-w-[150px]"}`}>{stat.name}</p>
+            </div>);
+          })}
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,29 +20,29 @@ export default function Home() {
            Results-driven Full Stack Engineer with experience in multiple languages.
            I deliver high-quality software solutions that exceed user expectations.
            </p>
-           <div className="flex items-center justify-center">  
+           <div className="flex flex-col xl:flex-row items-center gap-4">  
             <Button variant="outline" size='lg' 
             className="uppercase flex items-center gap-2 text-accent hover:text-white/50 rounded-[30px]">
               <span>Download cv</span>
               <FiDownload className="text-xl"/>
             </Button>
-           </div>
 
-           <div className=" mt-1 mb-8 xl:mb-0 flex items-center justify-center">
-            <Social containerStyles="flex gap-14"
+            <div className=" mt-1 mb-8 xl:mb-0 flex items-center justify-center">
+            <Social containerStyles="flex gap-5"
             iconStyles= "w-9 h-9 border border-accent rounded-[40px] flex justify-center items-center 
             text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
             />
            </div>
-           
+           </div>
         </div>
 
         <div className="order-1 xl:order-none mb-5">
           <Profile/>
         </div>
       </div>
-      </div>
       <Stats/>
+      </div>
+      
     </section>
   );
 }
