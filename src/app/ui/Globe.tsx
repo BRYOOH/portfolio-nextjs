315e2c3241bfb2c0ed9eaebaa70f19e78e5b3beb
BRYOOH <bryoohmuchira@gmail.com>
2024-11-04 07:11:27 +0300

    Deploy:GlobeErrors

--- src/app/ui/Globe.tsx
+++ src/app/ui/Globe.tsx
@@ -17,6 +17,14 @@ const RING_PROPAGATION_SPEED = 3;
 const aspect = 1.2;
 const cameraZ = 300;
 
+interface Point {
+  size: number;
+  order: number;
+  color: (t: number) => string;
+  lat: number;
+  lng: number;
+}
+
 type Position = {
   order: number;
   startLat: number;
@@ -115,7 +123,7 @@ export function Globe({ globeConfig, data }: WorldProps) {
 
   const _buildData = () => {
     const arcs = data;
-    let points = [];
+    let points:Point[] = [];
     for (let i = 0; i < arcs.length; i++) {
       const arc = arcs[i];
       const rgb = hexToRgb(arc.color) as { r: number; g: number; b: number };
@@ -295,7 +303,7 @@ export function hexToRgb(hex: string) {
 }
 
 export function genRandomNumbers(min: number, max: number, count: number) {
-  const arr = [];
+  const arr: number[] = [];
   while (arr.length < count) {
     const r = Math.floor(Math.random() * (max - min)) + min;
     if (arr.indexOf(r) === -1) arr.push(r);
